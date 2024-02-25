6c7ac2d6ac2ed223676a9ab3eab1d6dbb00aace5
dirtyweaselman <james.hackerr@gmail.com>
2024-02-25 02:22:42 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,163 @@
+print("2024 drtyweasl, joshfold. Licensed under the Unlicense license.")
+print("   ")
+print("Please enter which currency you would like to convert to: ")
+print("1. Bulwickian Krïna (type BWK)")
+print("2. Acemetarian Boner (type ACB)")
+print("3. Bloominian Hilch (type BHC)")
+print("4. Karbinatedbeanen Larog (type KBL)")
+print("5. Camel Pound (type CMP)")
+print("6. Chinadan Xuan (type CDX)")
+print("7. Garian Coin (type GAC)")
+print("8. Gingerland Ginger (type GGR)")
+print("9. Vian Via (type VIA)")
+print("10. Limardan Rupee (type LRP)")
+print("11. Lingaland Linga (type LIN)")
+print("12. Niroshi Yino (type NIR)")
+print("13. Rainiensafra Farat (type RAI)")
+print("14. Rizzistan Rizzpee (type RIZZ)")
+print("15. Evanovan Dollar (type EVA)")
+print("16. Rutavish Ruvet (type RUT)")
+print("17. Soisabu Shilling (type SOI)")
+print("18. Steida Dollar (type STE)")
+print("19. Tyroniean Chickennugget (type TYR)")
+print("20. Waarnistani Wyf (type WAA)")
+print("21. Zimzamian Zim (type ZIM)")
+print("22. Pelalan Podzol (type PEL)")
+
+currency = input().upper()
+
+if currency == "BWK":
+    print("Welcome to the USD to Krina Converter. Enter amount in USD:", end=" ")
+    usd = float(input())
+    usd_to_krina = 1.34
+    krina = usd * usd_to_krina
+    print(f"Amount in Krina: {krina}")
+elif currency == "ACB":
+    print("Welcome to the USD to Boner Converter. Enter amount in USD:", end=" ")
+    usd = float(input())
+    usd_to_boner = 420.69
+    acb = usd * usd_to_boner
+    print(f"Amount in Boner: {acb}")
+elif currency == "BHC":
+    print("Welcome to the USD to Hilch Converter. Enter amount in USD:", end=" ")
+    usd = float(input())
+    usd_to_hilch = 1.16
+    bhc = usd * usd_to_hilch
+    print(f"Amount in Hilch: {bhc}")
+elif currency == "KBL":
+    print("Welcome to the USD to Larog Converter. Enter amount in USD:", end=" ")
+    usd = float(input())
+    usd_to_larog = 0.67
+    kbl = usd * usd_to_larog
+    print(f"Amount in larog: {kbl}")
+elif currency == "CMP":
+    print("Welcome to the USD to Camel Pound Converter. Enter amount in USD:", end=" ")
+    usd = float(input())
+    usd_to_cmp = 22.6
+    cmp = usd * usd_to_cmp
+    print(f"Amount in Camel Pounds: {cmp}")
+elif currency == "CDX":
+    print("Welcome to the USD to Chinadan Xuan Converter. Enter amount in USD:", end=" ")
+    usd = float(input())
+    usd_to_cdx = 2.01
+    cdx = usd * usd_to_cdx
+    print(f"Amount in Xuan: {cdx}")
+elif currency == "GAC":
+    print("Welcome to the USD to Garian Coin Converter. Enter amount in USD:", end=" ")
+    usd = float(input())
+    usd_to_gac = 1.06
+    gac = usd * usd_to_gac
+    print(f"Amount in Garian Coin: {gac}")
+elif currency == "GGR":
+    print("Welcome to the USD to Gingerland Ginger Converter. Enter amount in USD:", end=" ")
+    usd = float(input())
+    usd_to_ggr = 1354.86
+    ggr = usd * usd_to_ggr
+    print(f"Amount in Gingerland Ginger: {ggr}")
+elif currency == "VIA":
+    print("Welcome to the USD to Vian Via Converter. Enter amount in USD:", end=" ")
+    usd = float(input())
+    usd_to_via = 5.3
+    via = usd * usd_to_via
+    print(f"Amount in Vian Via: {via}")
+elif currency == "LRP":
+    print("Welcome to the USD to Limardan Rupee. Enter amount in USD:", end=" ")
+    usd = float(input())
+    usd_to_lrp = 70.2
+    lrp = usd * usd_to_lrp
+    print(f"Amount in Limardan Rupee: {lrp}")
+elif currency == "LIN":
+    print("Welcome to the USD to Lingaland Linga. Enter amount in USD:", end=" ")
+    usd = float(input())
+    usd_to_lin = 9.02
+    lin = usd * usd_to_lin
+    print(f"Amount in Limardan Rupee: {lin}")
+elif currency == "NIR":
+    print("Welcome to the USD to Niroshi Yino. Enter amount in USD:", end=" ")
+    usd = float(input())
+    usd_to_yino = 2.08
+    yino = usd * usd_to_yino
+    print(f"Amount in Niroshi Yino: {yino}")
+elif currency == "RAI":
+    print("Welcome to the USD to Rainiensafra Farat. Enter amount in USD:", end=" ")
+    usd = float(input())
+    usd_to_farat = 23.54
+    farat = usd * usd_to_farat
+    print(f"Amount in Rainiensafra Farat: {farat}")
+elif currency == "RIZZ":
+    print("Welcome to the USD to Rizzistan Rizzpee. Enter amount in USD:", end=" ")
+    usd = float(input())
+    usd_to_rizzpee = 278.80
+    rizzpee = usd * usd_to_rizzpee
+    print(f"Amount in Rizzistan Rizzpee: {rizzpee}")
+elif currency == "EVA":
+    print("Welcome to the USD to Evanovan Dollar. Enter amount in USD:", end=" ")
+    usd = float(input())
+    usd_to_dollar = 4.13
+    dollar = usd * usd_to_dollar
+    print(f"Amount in Evanovan Dollar: {dollar}")
+elif currency == "RUT":
+    print("Welcome to the USD to Rutavish Ruvet. Enter amount in USD:", end=" ")
+    usd = float(input())
+    usd_to_rut = 9.0
+    rut = usd * usd_to_rut
+    print(f"Amount in Rutavish Ruvet: {rut}")
+elif currency == "SOI":
+    print("Welcome to the USD to Soisabu Shilling. Enter amount in USD:", end=" ")
+    usd = float(input())
+    usd_to_soi = 4.68
+    soi = usd * usd_to_soi
+    print(f"Amount in Soisabu Shilling: {soi}")
+elif currency == "STE":
+    print("Welcome to the USD to Steida Dollar. Enter amount in USD:", end=" ")
+    usd = float(input())
+    usd_to_ste = 1.03
+    ste = usd * usd_to_ste
+    print(f"Amount in Steida Dollar: {ste}")
+elif currency == "TYR":
+    print("Welcome to the USD to Tyroniean Chickennugget. Enter amount in USD:", end=" ")
+    usd = float(input())
+    usd_to_tyr = 42309423.5
+    tyr = usd * usd_to_tyr
+    print(f"Amount in Tyroniean Chickennugget: {tyr}")
+elif currency == "WAA":
+    print("Welcome to the USD to Waarnistani Wyf. Enter amount in USD:", end=" ")
+    usd = float(input())
+    usd_to_waa = 6.87
+    waa = usd * usd_to_waa
+    print(f"Amount in Waarnistani Wyf: {waa}")
+elif currency == "ZIM":
+    print("Welcome to the USD to Zimzamian Zim. Enter amount in USD:", end=" ")
+    usd = float(input())
+    usd_to_zim = 4.20
+    zim = usd * usd_to_zim
+    print(f"Amount in Zimzamian Zim: {zim}")
+elif currency == "PEL":
+    print("Welcome to the USD to Pelalan Podzol. Enter amount in USD:", end=" ")
+    usd = float(input())
+    usd_to_pel = 6.62
+    pel = usd * usd_to_pel
+    print(f"Amount in Pelalan Podzol: {pel}")
+
+
+input("Press enter to exit.")
